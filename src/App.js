325669c9c6a87bc12a6599aa9977b25e199aab89
hjkim <dudnwjs@gmail.com>
2018-02-18 03:20:04 +0900

    graphql server 적용

--- src/App.js
+++ src/App.js
@@ -2,28 +2,41 @@ import React, { Component } from 'react';
 
 class App extends Component {
   state = {
-    response: ''
+    response: []
   };
 
   componentDidMount() {
     this.callApi()
-      .then(res => this.setState({ response: res.express }))
+      .then(res => this.setState({ response: res.data.findbook }))
       .catch(err => console.log(err));
   }
 
   callApi = async () => {
-    const response = await fetch('/api/hello');
+    const ql = `{
+      findbook(name:"hwkim") {
+        title
+        author
+      }
+    }`
+    const booksql = `{
+      books{
+        title
+        author
+      }
+    }`
+    const response = await fetch('/graphql?query='+ql);
     const body = await response.json();
+    console.log(body);
 
     if (response.status !== 200) throw Error(body.message);
-
     return body;
   };
   render() {
+    const listBooks = this.state.response.map((book,i) => <li key={i}>TITLE: {book.title} -> AUTHOR: {book.author}</li>)
     return (
       <div>
         HI. CrytoDogs
-        <p className="App-intro">{this.state.response}</p>
+        <ul>{listBooks}</ul>
       </div>
     );
   }
--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,74 @@
-const express = require('express');
+import express from 'express'
+import bodyParser from 'body-parser'
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
+import { makeExecutableSchema } from 'graphql-tools'
+import mongoose from 'mongoose';
+
+mongoose.connect("mongodb://test:test@ds123658.mlab.com:23658/dudnwjsdb")
+  .then( () => {
+    console.log("몽구스 연결 성공")
+  }).catch((err) => {
+    console.log("몽구스 연결 실패")
+  })
+
+const port = process.env.PORT || 5000
+
+// const db_books = [
+//   {title:"ThisIsABook", author:"hjkim"},
+//   {title:"ThisIsBBook", author:"hwkim"},
+//   {title:"ThisIsCBook", author:"hnkim"},
+//   {title:"ThisIsDBook", author:"hjkim"},
+// ]
+// db_books.forEach(item=>{
+//   console.log(item);
+// })
+const Book = mongoose.model("book",{title:String,author:String})
+const typeDefs = `
+  type Query{
+    books: [Book]
+    findbook(name:String): [Book]
+  }
+  type Book {
+    title: String
+    author: String
+  }
+  type Mutation{
+    addBook(title:String,author:String): Book
+  }
+`
+// (parent, arguments, context)
+const resolvers = {
+  Query: {
+    books: async (obj,args,ctx) => {
+      return await ctx.book.find()
+    },
+    findbook: async (obj,args,ctx) => {
+      return await ctx.book.find({author:args.name})
+    }
+  },
+  Mutation: {
+    addBook: async (obj,args,ctx) => {
+      console.log(args)
+      return await new ctx.book(args).save();
+    }
+  }
+}
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+})
 
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+app.use("/graphql",bodyParser.json(),graphqlExpress({schema,context:{book:Book}}));
+
+app.use("/graphiql", graphiqlExpress({endpointURL:"/graphql"}));
+
+app.get('/api/hello', (req,res) => {
+  res.send({ express:'dsffsdf from Express'});
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port ,() => {
+  console.log("서버가 시작되었습니다.!");
+})
