5f46d12a5e5b926d2d6b0baba7e21b63f508c205
hjkim86 <hjkim86@sptek.co.kr>
2018-02-23 18:31:03 +0900

    add style(materialize)

--- src/App/views/CreateView.js
+++ src/App/views/CreateView.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 import { Redirect } from 'react-router-dom'
+import { Button, Icon, Card, Input } from 'react-materialize'
 
 const query = gql`
 mutation AddBook($title: String!, $author: String!) {
@@ -14,14 +15,28 @@ class CreateView extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      redirect: false
+      redirect: false,
+      title: 'state_title',
+      author: 'state_author'
     }
+    this.handleTitle = this.handleTitle.bind(this)
+    this.handleAuthor = this.handleAuthor.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleAuthor(event) {
+    console.log(event.target.value)
+    this.setState({ author: event.target.value })
+  }
+
+  handleTitle(event) {
+    console.log(event.target.value)
+    this.setState({ title: event.target.value })
   }
 
   handleSubmit(event) {
     event.preventDefault()
-    this.props.mutate({ variables: { title: 'title_create', author: 'author_create' } })
+    this.props.mutate({ variables: { title: this.state.title, author: this.state.author } })
       .then(() => this.setState({ redirect: true }))
   }
 
@@ -34,7 +49,11 @@ class CreateView extends React.Component {
       <div>
         <h1>CreateView</h1>
         <form onSubmit={this.handleSubmit}>
-          <button type="submit">Create</button>
+          <Input s={6} label="Book Title" onChange={this.handleTitle}/>
+          <Input s={6} label="Book Author" onChange={this.handleAuthor}/>
+          <Button type="submit" waves='light'>
+              Create
+          </Button>
         </form>
       </div>
     )
